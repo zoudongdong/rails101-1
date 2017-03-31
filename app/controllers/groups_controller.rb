ed5a77a25ee3e726c48701105d286c064a265e3c
zoudongdong <alpeveryday@gmail.com>
2017-03-31 17:39:27 +0800

    inplement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,10 +3,14 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
-  
+
   def create
     @group = Group.new(group_params)
     @group.save
